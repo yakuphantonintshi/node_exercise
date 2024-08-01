90da28324002019619d5a29366a07a42b21d29a0
yakuphantonintshi <yakupha58756@gmail.com>
2024-08-01 14:43:06 +0200

    Product Content

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// 
+import express from 'express'
+import path from 'path'
+import { config} from 'dotenv'
+config()
+
+const app = express()
+const port = +process.env.PORT || 4000
+app.use(express.static('./static'))
+
+app.get('/', (req,res) => {
+    res.status(200).sendFile(path.resolve('./static/index.html'))
+})
+
+app.get('/about', (req,res) => {
+    res.status(200).sendFile(path.resolve('./static/about.html'))
+})
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`);
+})
